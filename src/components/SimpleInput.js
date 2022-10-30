f637c198bc7cd84f372334591aa49505988dcb38
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-10-30 17:16:24 +0530

    feat: check input validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,10 +1,29 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [name, setName] = useState('');
+  const [nameIsValid, setNameIsValid] = useState(true);
+  const nameRef = useRef();
+  const handleOnChangeName = event => {
+    setName(event.target.value);
+  }
+  const handleOnSubmit = event => {
+    event.preventDefault();
+    if(name.trim() === ''){
+      setNameIsValid(false);
+      return;
+    }
+    setNameIsValid(true);
+    console.log('STATE ',name);
+    console.log('REF ',nameRef.current.value);
+  }
   return (
-    <form>
+    <form onSubmit={handleOnSubmit}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' ref={nameRef} onChange={handleOnChangeName}/>
       </div>
+      { !nameIsValid && <p>Entered name is invalid!</p>}
       <div className="form-actions">
         <button>Submit</button>
       </div>
