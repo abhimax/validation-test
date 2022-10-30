0d303b720a2d8d7d772bdda4b6b88493ecc54548
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-10-30 17:51:28 +0530

    feat: fix input validation issue

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,29 +1,43 @@
 import { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
   const [nameIsValid, setNameIsValid] = useState(true);
+  const [enteredNameIsTouched, setEnteredNameTouched] = useState(false);
   const nameRef = useRef();
-  const handleOnChangeName = event => {
+  const handleOnChangeName = (event) => {
     setName(event.target.value);
-  }
-  const handleOnSubmit = event => {
+  };
+  const handleOnSubmit = (event) => {
     event.preventDefault();
-    if(name.trim() === ''){
+    setEnteredNameTouched(true);
+    if (name.trim() === "") {
       setNameIsValid(false);
       return;
     }
     setNameIsValid(true);
-    console.log('STATE ',name);
-    console.log('REF ',nameRef.current.value);
-  }
+    console.log("STATE ", name);
+    console.log("REF ", nameRef.current.value);
+  };
+
+  const enteredNameIsInvalid = !nameIsValid && enteredNameIsTouched;
+  const nameInputFormClass = enteredNameIsInvalid
+    ? "form-control invalid"
+    : "form-control";
   return (
     <form onSubmit={handleOnSubmit}>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' ref={nameRef} onChange={handleOnChangeName}/>
+      <div className={nameInputFormClass}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          ref={nameRef}
+          onChange={handleOnChangeName}
+        />
       </div>
-      { !nameIsValid && <p>Entered name is invalid!</p>}
+      {enteredNameIsInvalid && (
+        <p className="error-text">Entered name is invalid!</p>
+      )}
       <div className="form-actions">
         <button>Submit</button>
       </div>
